a28017ac3f9648f43e62ab1b4f1c28432e6a1f82
TopherGacad <Gacadtophs@gmail.com>
2023-05-16 13:22:31 +0800

    fix the problem with the js file

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,45 +1,45 @@
 // MAIN CONTENT CONTAINERS
 const dashContain = document.getElementById("dash-container")
-const leavesContain = document.getElementById("leaves-container")
+const shiftsContain = document.getElementById("shifts-container")
 const overtimeContain = document.getElementById("overtime-container")
 
 //BUTTONS
 const dashBtn = document.getElementById("dash-btn")
-const leavesBtn = document.getElementById("leaves-btn")
+const shiftsBtn = document.getElementById("shifts-btn")
 const overtimeBtn = document.getElementById("overtime-btn")
 
 // MAIN CONTENTS CONTAINER SELECTOR
 dashBtn.addEventListener("click", function(){
     //MAIN CONTENT ACTIVATOR
     dashContain.style.display = "block"
-    leavesContain.style.display = "none"
+    shiftsContain.style.display = "none"
     overtimeContain.style.display = "none"
 
     //BUTTON ACTIVE STYLING
     dashBtn.classList.add('btn-active')
-    leavesBtn.classList.remove('btn-active')
+    shiftsBtn.classList.remove('btn-active')
     overtimeBtn.classList.remove('btn-active')
 })
-leavesBtn.addEventListener("click", function(){
+shiftsBtn.addEventListener("click", function(){
     //MAIN CONTENT ACTIVATOR
     dashContain.style.display = "none"
-    leavesContain.style.display = "block"
+    shiftsContain.style.display = "block"
     overtimeContain.style.display = "none"
 
     //BUTTON ACTIVE STYLING
     dashBtn.classList.remove('btn-active')
-    leavesBtn.classList.add('btn-active')
+    shiftsBtn.classList.add('btn-active')
     overtimeBtn.classList.remove('btn-active')
 })
 overtimeBtn.addEventListener("click", function(){
     //MAIN CONTENT ACTIVATOR
     dashContain.style.display = "none"
-    leavesContain.style.display = "none"
+    shiftsContain.style.display = "none"
     overtimeContain.style.display = "block"
 
     //BUTTON ACTIVE STYLING
     dashBtn.classList.remove('btn-active')
-    leavesBtn.classList.remove('btn-active')
+    shiftsBtn.classList.remove('btn-active')
     overtimeBtn.classList.add('btn-active')
 })
 
