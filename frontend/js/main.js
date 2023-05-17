5d2427521fdc84fff7661acf458e2f5293a1670c
TopherGacad <Gacadtophs@gmail.com>
2023-05-17 15:29:18 +0800

    add the layout for shift and ob module

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,11 +2,13 @@
 const dashContain = document.getElementById("dash-container")
 const shiftsContain = document.getElementById("shifts-container")
 const overtimeContain = document.getElementById("overtime-container")
+const offBusContain = document.getElementById("offBusiness-container")
 
 //BUTTONS
 const dashBtn = document.getElementById("dash-btn")
 const shiftsBtn = document.getElementById("shifts-btn")
 const overtimeBtn = document.getElementById("overtime-btn")
+const offBusBtn = document.getElementById("offBusiness-btn")
 
 //MODAL BACKGROUND
 const modalBg = document.getElementById("bg")
@@ -17,33 +19,52 @@ dashBtn.addEventListener("click", function(){
     dashContain.style.display = "block"
     shiftsContain.style.display = "none"
     overtimeContain.style.display = "none"
+    offBusContain.style.display = "none"
 
     //BUTTON ACTIVE STYLING
     dashBtn.classList.add('btn-active')
     shiftsBtn.classList.remove('btn-active')
     overtimeBtn.classList.remove('btn-active')
+    offBusBtn.classList.remove('btn-active')
 })
 shiftsBtn.addEventListener("click", function(){
     //MAIN CONTENT ACTIVATOR
     dashContain.style.display = "none"
     shiftsContain.style.display = "block"
     overtimeContain.style.display = "none"
+    offBusContain.style.display = "none"
 
     //BUTTON ACTIVE STYLING
     dashBtn.classList.remove('btn-active')
     shiftsBtn.classList.add('btn-active')
     overtimeBtn.classList.remove('btn-active')
+    offBusBtn.classList.remove('btn-active')
 })
 overtimeBtn.addEventListener("click", function(){
     //MAIN CONTENT ACTIVATOR
     dashContain.style.display = "none"
     shiftsContain.style.display = "none"
     overtimeContain.style.display = "block"
+    offBusContain.style.display = "none"
 
     //BUTTON ACTIVE STYLING
     dashBtn.classList.remove('btn-active')
     shiftsBtn.classList.remove('btn-active')
     overtimeBtn.classList.add('btn-active')
+    offBusBtn.classList.remove('btn-active')
+})
+offBusBtn.addEventListener("click",function(){
+     //MAIN CONTENT ACTIVATOR
+     dashContain.style.display = "none"
+     shiftsContain.style.display = "none"
+     overtimeContain.style.display = "none"
+     offBusContain.style.display = "block"
+ 
+     //BUTTON ACTIVE STYLING
+     dashBtn.classList.remove('btn-active')
+     shiftsBtn.classList.remove('btn-active')
+     overtimeBtn.classList.remove('btn-active')
+     offBusBtn.classList.add('btn-active')
 })
 
 const overtimeModal = document.getElementById("overtime-modal-container")
